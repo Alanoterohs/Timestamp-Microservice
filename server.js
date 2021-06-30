55713de07cf8445f20b5f6ca1e35bbd7adafe721
Alanoterohs <alanoterohs@gmail.com>
2021-06-29 22:13:53 -0300

    finished project

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(express.json());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,14 +20,32 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:number', (req, res) => {
+  const param = req.params.number;
 
+  const jsonToDate = new Date(param).toLocaleDateString();
 
-// listen for requests :)
+  if ((/^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/).test(jsonToDate)) {
+    res.json({
+      unix: Date.parse(param),
+      utc: new Date(param).toDateString() + ' ' + new Date(param).toTimeString()
+    });
+  } else {
+    const jsonToNumber = JSON.parse(param);
+    const parseUnix = new Date(jsonToNumber).toDateString() + ' ' + new Date(jsonToNumber).toTimeString();
+    res.json({
+      unix: param,
+      utc: parseUnix,
+    });
+  }
+});
+
+//listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
